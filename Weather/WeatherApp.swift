4a1b5a752116a722508765794290fd9459f1932e
fbocolowski <fbocolowski@gmail.com>
2021-09-17 14:28:12 -0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Felipe Bocolowski on 17/09/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
